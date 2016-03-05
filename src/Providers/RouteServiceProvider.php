da19f29c07361c65c06c1381e56b50c804217379
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2016-03-06 00:46:31 +0200

    Cleanup RouteServiceProvider

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace RabbitCMS\Carrot\Providers;
 
+use Illuminate\Routing\Router;
+
 class RouteServiceProvider extends ServiceProvider
 {
     /**
@@ -21,11 +23,11 @@ class RouteServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot(Router $router)
-{
-    //
+    {
+        //
 
-    parent::boot($router);
-}
+        parent::boot($router);
+    }
 
     /**
      * Define the routes for the application.
@@ -35,48 +37,24 @@ class RouteServiceProvider extends ServiceProvider
      * @return void
      */
     public function map(Router $router)
-{
-    $router->group(['middleware' => ['api']], function (Router $router) {
-        // Create an instance of JsonRpcServer
-        /* @var ServerContract $jsonRpcServer */
-        $jsonRpcServer = $this->app->make(ServerContract::class);
-        // Set default controller namespace
-        //$jsonRpcServer->setControllerNamespace($this->namespace);
-        // Register middleware aliases configured for Laravel router
-        $jsonRpcServer->registerMiddlewareAliases($router->getMiddleware());
+    {
+        $router->group(['middleware' => ['api']], function (Router $router) {
 
-        $router->post(
-            '/jsonrpc',
-            function (Request $request) use ($jsonRpcServer) {
-                // Run json-rpc server with $request passed to middlewares as a handle() method argument
-                return $jsonRpcServer->run($request);
-            }
-        );
-        $router->group(
-            ['namespace' => $this->namespace],
-            function (Router $router) use ($jsonRpcServer) {
-                require app_path('Http/routes.php');
-            }
-        );
-        foreach ($this->app->make('modules')->getOrdered() as $module) {
-            /* @var \Pingpong\Modules\Module $module */
-            $autoload = $module->getComposerAttr('autoload');
-            if (array_key_exists('psr-4', $autoload) && is_array($autoload['psr-4']) && count($autoload['psr-4']) > 0) {
-                $namespace = key($autoload['psr-4']).'\Http\Controllers';
-            } else {
-                $namespace = '';
-            }
-
-            $jsonRpcServer->router()->group(
-                ['namespace' => $namespace],
-                function (\DKulyk\JsonRpc\Server\Router $router) use ($module, $jsonRpcServer) {
-                    $path = $module->getExtraPath('Http/routes.php');
-                    if (is_file($path)) {
-                        require $path;
-                    }
+            $router->group(
+                ['namespace' => $this->namespace],
+                function (Router $router) use ($jsonRpcServer) {
+                    require app_path('Http/routes.php');
                 }
             );
-        }
-    });
-}
+            foreach ($this->app->make('modules')->getOrdered() as $module) {
+                /* @var \Pingpong\Modules\Module $module */
+                $autoload = $module->getComposerAttr('autoload');
+                if (array_key_exists('psr-4', $autoload) && is_array($autoload['psr-4']) && count($autoload['psr-4']) > 0) {
+                    $namespace = key($autoload['psr-4']).'\Http\Controllers';
+                } else {
+                    $namespace = '';
+                }
+            }
+        });
+    }
 }
