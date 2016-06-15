c09e38b12524b30bdf25b1b220b1e6c1ba8074e4
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2016-06-16 02:27:50 +0300

    Add AclToGate service provider.

---
+++ src/Providers/AclToGateProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace RabbitCMS\Carrot\Providers;
+
+use Illuminate\Contracts\Auth\Access\Gate;
+use Illuminate\Support\ServiceProvider;
+use RabbitCMS\Carrot\Contracts\HasAccessEntity;
+use RabbitCMS\Carrot\Repository\BackendAcl;
+
+class AclToGateProvider extends ServiceProvider
+{
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Define acl as gate permissions.
+     *
+     * @param \Illuminate\Contracts\Auth\Access\Gate  $gate
+     * @param \RabbitCMS\Carrot\Repository\BackendAcl $acl
+     */
+    public function boot(Gate $gate, BackendAcl $acl)
+    {
+        foreach ($acl->getAll() as $acl => $label) {
+            $gate->define(
+                $acl,
+                function (HasAccessEntity $user) use ($acl) {
+                    return $user->hasAccess($acl);
+                }
+            );
+        }
+    }
+}
