91b37a26b0fd0bfb201f6024bbf45b5249793e89
Dmytro Zhurakovskyi <dmytro.zhurakovsky@gmail.com>
2016-09-20 17:36:55 +0300

    Legacy modules support

---
+++ src/Http/ModuleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RabbitCMS\Carrot\Http;
+
+/**
+ * Class ModuleController.
+ *
+ * @deprecated Legacy modules support
+ */
+class ModuleController extends \RabbitCMS\Modules\ModuleController
+{
+
+}
