7d024837ede3e29db74c0840176fdb4634c35c2d
Kulyk Dmytro <lnkvisitor.ts@gmail.com>
2016-12-13 09:15:19 +0200

    Check order column

--- src/Support/Grid2.php
+++ src/Support/Grid2.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace RabbitCMS\Carrot\Support;
 
 use Illuminate\Database\Eloquent\Builder;
@@ -7,20 +7,42 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * Class Grid2.
+ */
 abstract class Grid2
 {
+    /**
+     * @return Eloquent
+     */
     abstract public function getModel() :Eloquent;
 
+    /**
+     * @param Builder $query
+     * @param array   $filters
+     *
+     * @return Builder
+     */
     protected function filters(Builder $query, array $filters) :Builder
     {
         return $query;
     }
 
+    /**
+     * @param Eloquent $row
+     *
+     * @return array
+     */
     protected function prepareRow(Eloquent $row) :array
     {
         return $row->attributesToArray();
     }
 
+    /**
+     * @param Request|null $request
+     *
+     * @return JsonResponse
+     */
     public function response(Request $request = null) :JsonResponse
     {
         $request = $request ?: request();
@@ -39,17 +61,20 @@ abstract class Grid2
             return $this->prepareRow($row);
         });
 
-        return new JsonResponse(
-            [
-                'recordsTotal'    => $total,
-                'recordsFiltered' => $count,
-                'draw'            => $request->input('draw'),
-                'data'            => $data,
-            ],
-            200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
-        );
+        return new JsonResponse([
+            'recordsTotal'    => $total,
+            'recordsFiltered' => $count,
+            'draw'            => $request->input('draw'),
+            'data'            => $data,
+        ], 200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     }
 
+    /**
+     * @param array $filters
+     * @param array $orders
+     *
+     * @return Builder
+     */
     public function getQuery(array $filters = [], array $orders = []) :Builder
     {
         $query = $this->filters($this->getModel()->newQuery(), $filters);
@@ -61,13 +86,18 @@ abstract class Grid2
         return $query;
     }
 
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
     public function getOrders(Request $request) :array
     {
         $result = [];
 
         $columns = $request->input('columns', []);
         foreach ((array)$request->input('order', []) as $order) {
-            if (array_key_exists($order['column'], $columns)) {
+            if (array_key_exists($order['column'], $columns) && !empty($columns[$order['column']]['name'])) {
                 $result[] = [
                     $columns[$order['column']]['name'],
                     $order['dir']
