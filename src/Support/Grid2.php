e6c7d892492ca4a79b26ad3278faec5ac50e9fd2
Dmytro Zhurakovskyi <dmytro.zhurakovsky@gmail.com>
2017-05-30 15:22:22 +0300

    Grid2 update

--- src/Support/Grid2.php
+++ src/Support/Grid2.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace RabbitCMS\Carrot\Support;
 
 use Illuminate\Database\Eloquent\Builder;
@@ -7,27 +7,67 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * Class Grid2.
+ */
 abstract class Grid2
 {
+    /**
+     * @var array
+     */
+    public static $handlers = [];
+
+    /**
+     * @param callable $handler
+     */
+    public static function addHandler(callable $handler)
+    {
+        self::$handlers[] = $handler;
+    }
+
+    /**
+     * @return Eloquent
+     */
     abstract public function getModel() :Eloquent;
 
+    /**
+     * @param Builder $query
+     * @param array   $filters
+     *
+     * @return Builder
+     */
     protected function filters(Builder $query, array $filters) :Builder
     {
         return $query;
     }
 
+    /**
+     * @param Eloquent $row
+     *
+     * @return array
+     */
     protected function prepareRow(Eloquent $row) :array
     {
         return $row->attributesToArray();
     }
 
+    /**
+     * @param Request|null $request
+     *
+     * @return JsonResponse
+     */
     public function response(Request $request = null) :JsonResponse
     {
         $request = $request ?: request();
         $total = $this->getModel()->newQuery()->count();
 
-        $query = $this->getQuery((array)$request->input('filters', []), $this->getOrders($request));
+        $query = $this->getQuery($request);
+
         $count = $query->count();
+
+        foreach ($this->getOrders($request) as $order) {
+            $query->orderBy(...$order);
+        }
 
         if ($request->input('length') > 0) {
             $query
@@ -39,35 +79,44 @@ abstract class Grid2
             return $this->prepareRow($row);
         });
 
-        return new JsonResponse(
-            [
-                'recordsTotal'    => $total,
-                'recordsFiltered' => $count,
-                'draw'            => $request->input('draw'),
-                'data'            => $data,
-            ],
-            200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
-        );
+        return new JsonResponse([
+            'recordsTotal' => $total,
+            'recordsFiltered' => $count,
+            'draw' => $request->input('draw'),
+            'data' => $data,
+        ], 200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     }
 
-    public function getQuery(array $filters = [], array $orders = []) :Builder
+    /**
+     * @param Request $request
+     *
+     * @return Builder
+     */
+    public function getQuery(Request $request) :Builder
     {
-        $query = $this->filters($this->getModel()->newQuery(), $filters);
+        $query = $this->filters($this->getModel()->newQuery(), (array)$request->input('filters', []));
 
-        foreach ($orders as $order) {
-            $query->orderBy(...$order);
-        }
+        $query->where(function (Builder $query) use ($request) {
+            array_map(function (callable $handler) use ($query, $request) {
+                call_user_func($handler, $query, $request);
+            }, self::$handlers);
+        });
 
         return $query;
     }
 
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
     public function getOrders(Request $request) :array
     {
         $result = [];
 
         $columns = $request->input('columns', []);
         foreach ((array)$request->input('order', []) as $order) {
-            if (array_key_exists($order['column'], $columns)) {
+            if (array_key_exists($order['column'], $columns) && !empty($columns[$order['column']]['name'])) {
                 $result[] = [
                     $columns[$order['column']]['name'],
                     $order['dir']
